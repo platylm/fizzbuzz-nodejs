addcf6a1737fa10d5d7e4264d32e41196a16020f
platylm <n_nareenart@kkumail.com>
2020-01-07 10:23:55 +0700

    [Edited]: ปรับชื่อ function ด้านในของ function FizzBuzz ใน if

--- fizzbuzz.js
+++ fizzbuzz.js
@@ -4,9 +4,9 @@ function isBuzz(number) {return number%5 === 0}
 function isFizzBuzz(number) {return isFizz(number) && isBuzz(number)}
 
 function FizzBuzz(number) {
-    if (CallFizzBuzz(number)) return "FizzBuzz"
-    if (CallFizz(number)) return "Fizz"
-    if (CallBuzz(number)) return "Buzz"
+    if (isFizzBuzz(number)) return "FizzBuzz"
+    if (isFizz(number)) return "Fizz"
+    if (isBuzz(number)) return "Buzz"
     return number
 }
 module.exports = FizzBuzz //จะประกาศ module ข้างล่าง function นั้น
